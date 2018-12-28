7b570989133635a3301f2a6450b4ce54ad7a280d
tsachiblau <tsachiblau@gmail.com>
2018-12-28 23:46:44 +0200

    `

--- main.m
+++ main.m
@@ -3,7 +3,13 @@
 day=1;
 subject=1;
 [Events, vClass] = GetEvents(subject,day);
-cov_of_all_events=cov_from_cellarray_of_events(Events);
-group1_cov;
-P1=riemannianMean(group1_cov,step,epsilon,max_num_of_iter);
-P2=riemannianMean(group2_cov,step,epsilon,max_num_of_iter);
+cov_of_all_events=covFromCellArrayOfEvents(Events);
+group1=1;
+group2=2;
+group1_cov=getCovOfGroup(cov_of_all_events,vClass,group1);          %need to implement
+group2_cov=getCovOfGroup(cov_of_all_events,vClass,group2);
+
+mean1=riemannianMean(group1_cov,step,epsilon,max_num_of_iter);
+mean2=riemannianMean(group2_cov,step,epsilon,max_num_of_iter);
+dist1=calcDistanceBetweenOneCovAndLotOfCov(mean1,group1_cov);
+dist2=calcDistanceBetweenOneCovAndLotOfCov(mean2,group2_cov);       %need to implement
