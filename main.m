ed2ae6bc5c9bd2cb78ac3b6079cd33882a95465b
tsachiblau <tsachiblau@gmail.com>
2018-12-29 12:43:32 +0200

    new param for main

--- main.m
+++ main.m
@@ -8,6 +8,9 @@ group1=1;
 group2=2;
 group1_cov=cov_of_all_events(vClass==group1,:,:);  
 group2_cov=cov_of_all_events(vClass==group2,:,:);
+step=1e-4;
+epsilon=1;
+max_num_of_iter=100;
 
 mean1=riemannianMean(group1_cov,step,epsilon,max_num_of_iter);
 mean2=riemannianMean(group2_cov,step,epsilon,max_num_of_iter);
