76a9c1cd1446677dc76fd6ea30b34a22ec1a3ee0
tsachiblau <tsachiblau@gmail.com>
2018-12-29 12:14:08 +0200

    new main

--- main.m
+++ main.m
@@ -6,8 +6,8 @@ subject=1;
 cov_of_all_events=covFromCellArrayOfEvents(Events);
 group1=1;
 group2=2;
-group1_cov=getCovOfGroup(cov_of_all_events,vClass,group1);          %need to implement
-group2_cov=getCovOfGroup(cov_of_all_events,vClass,group2);
+group1_cov=cov_of_all_events(vClass==group1,:,:);  
+group2_cov=cov_of_all_events(vClass==group2,:,:);
 
 mean1=riemannianMean(group1_cov,step,epsilon,max_num_of_iter);
 mean2=riemannianMean(group2_cov,step,epsilon,max_num_of_iter);
