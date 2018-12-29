005c02fbf6e472ce18ad6dbcd4d184bd0df6f613
katiePodiatchev <katy1427@gmail.com>
2018-12-29 14:08:57 +0200

    draw initial mean from random distribution

--- riemannianMean.m
+++ riemannianMean.m
@@ -2,8 +2,7 @@ function [P] = riemannianMean(group_cov,step,epsilon,max_num_of_iter)
     d = 2*epsilon;
     j=0;
     [N,~,n] = size(group_cov);
-    P = ones(n);
-    delta = ones(n);
+    P = 0.1*rand(n,n);
     S = projectToTangentSpace(P,group_cov);
     while d>epsilon && j<max_num_of_iter
         delta = sum(S,1);
