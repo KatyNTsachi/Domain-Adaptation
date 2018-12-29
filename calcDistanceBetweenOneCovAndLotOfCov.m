a8025fa40252860b15eb9b27f689521ee3e40126
tsachiblau <tsachiblau@gmail.com>
2018-12-29 17:17:32 +0200

    working version!

--- calcDistanceBetweenOneCovAndLotOfCov.m
+++ calcDistanceBetweenOneCovAndLotOfCov.m
@@ -4,11 +4,12 @@ function [ret_distance] = calcDistanceBetweenOneCovAndLotOfCov(mean,group_cov)
     num_of_iter=size(group_cov,1);
     inv_mean=inv(mean);
     
-    parfor i=1:num_of_iter
+    for i=1:num_of_iter
         tmp_cov=squeeze(group_cov(i,:,:));
         in_log=inv_mean*tmp_cov;
         eig_value=eig(in_log);
         eig_value_log=log(eig_value);
+        eig_value_log=eig_value_log.^2;
         eig_value_log_sum=sum(eig_value_log);
         eig_value_log_sum_sqrt=sqrt(eig_value_log_sum);
         ret_distance(i)=eig_value_log_sum_sqrt;
--- riemannianMean.m
+++ riemannianMean.m
@@ -14,6 +14,11 @@ function [P] = riemannianMean(group_cov,step,epsilon,max_num_of_iter)
         S_mean = mean(S,1);
         S_mean=squeeze(S_mean);
         P = projectToRiemannianSpace(P,S_mean);
+        
+        tmp_dist=calcDistanceBetweenOneCovAndLotOfCov(P,group_cov);
+        j=j
+        tmp_dist=sum(tmp_dist)
+        
         d = det(P*P');
         %P=P-step*delta;
         j=j+1;
