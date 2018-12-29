de819543e02234e330f947c3f2661fcf11a6eebb
katiePodiatchev <katy1427@gmail.com>
2018-12-29 13:53:54 +0200

    tmp

--- riemannianMean.m
+++ riemannianMean.m
@@ -1,13 +1,18 @@
 function [P] = riemannianMean(group_cov,step,epsilon,max_num_of_iter)
     d = 2*epsilon;
     j=0;
-    [~,~,n] = size(group_cov);
+    [N,~,n] = size(group_cov);
     P = ones(n);
     delta = ones(n);
     S = projectToTangentSpace(P,group_cov);
     while d>epsilon && j<max_num_of_iter
+        delta = sum(S,1);
+        delta = squeeze(delta);
+        delta = 2*(delta - N*P);
+        
+        d = sum(delta,'all');
+        P=P-step*delta;
         j=j+1;
-        delta = step*(2*(S-P));
     end
 end
 
