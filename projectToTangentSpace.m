2dc5d4eba0b7833975eb4221063c90af58bb94ed
tsachiblau <tsachiblau@gmail.com>
2018-12-29 12:43:16 +0200

    fixed project to tangent space

--- projectToTangentSpace.m
+++ projectToTangentSpace.m
@@ -8,17 +8,18 @@ function [ret_p] = projectToTangentSpace(p,pi)
     p_minus_pow_half=mpower(p,-0.5);
     
     %is it list of mat or just one?
-    if size(size(s),2)==3
-        ret_p=zeros(size(s,1),size(s,2),size(s,3));
-        num_of_matrics=size(s,1);
+    if size(size(pi),2)==3
+        ret_p=zeros(size(pi,1),size(pi,2),size(pi,3));
+        num_of_matrics=size(pi,1);
     else
-        ret_p=zeros(size(s,1),size(s,2));
+        ret_p=zeros(size(pi,1),size(pi,2));
         num_of_matrics=0;
     end
     
     if num_of_matrics~=0
         parfor i=1:num_of_matrics
-            in_log=p_minus_pow_half*pi*p_minus_pow_half;
+            tmp_pi=squeeze(pi(i,:,:));
+            in_log=p_minus_pow_half*tmp_pi*p_minus_pow_half;
             log_mat=logm(in_log);
             ret_p(i,:,:)=p_pow_half*log_mat*p_pow_half;
         end
