abedc788a3ab61d0412e00d0edc35a4aaba455d1
katiePodiatchev <katy1427@gmail.com>
2019-01-19 13:01:08 +0200

    tmp

--- main.m
+++ main.m
@@ -10,7 +10,7 @@ class1            = 1;
 class2            = 2;
 class1_cov        = cov_of_all_events(:,:,vClass == class1);
 class2_cov        = cov_of_all_events(:,:,vClass == class2);
-
+two_cov           = cov_of_all_events(:,:, (vClass == class1 | vClass == class2) );
 epsilon           = 1e-6;
 max_iter          = 100;
 
@@ -30,3 +30,22 @@ axis equal;
 title('Reimanian Distance to Reimanian mean');
 xlabel('Reimanian distance to class 1 mean');
 ylabel('Reimanian distance to class 2 mean');
+
+%% svm
+two_mean                        = riemannianMean(two_cov, epsilon, max_iter);
+class1_cov_projected_to_two_cov = projectToTangentSpace(two_mean, class1_cov);
+class2_cov_projected_to_two_cov = projectToTangentSpace(two_mean, class2_cov); 
+num_of_features                 = size(class1_cov,1)*size(class1_cov,2);
+class1_cov_projected_to_two_cov = reshape(class1_cov_projected_to_two_cov, num_of_features,[]);
+class2_cov_projected_to_two_cov = reshape(class2_cov_projected_to_two_cov, num_of_features,[]);
+
+%calc svm
+X = [class1_cov_projected_to_two_cov,class2_cov_projected_to_two_cov]
+Y = [ ones( size(class1_cov_projected_to_two_cov,2),1 ); ones(size(class1_cov_projected_to_two_cov,2),1) * 2 ];
+Mdl = fitcsvm(X',Y)
+
+
+
+
+
+
