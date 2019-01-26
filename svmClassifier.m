70a878046d6a45081bffea76d784478e3a6076a0
katiePodiatchev <katy1427@gmail.com>
2019-01-26 10:33:33 +0200

    tmp

--- svmClassifier.m
+++ svmClassifier.m
@@ -7,45 +7,24 @@ subject           = 3;
 epsilon           = 1e-6;
 max_iter          = 100;
 
-%-- create forier Events 
-F_Events={};
+%-- Create Forier Events 
+F_Events={length(Events)};
 for ii=1:length(Events)
     F_Events{ii} = abs( fft( Events{ii} ) );
 end
 
-for base_func=["gaussian" "linear" "polynomial"]
-    
-    
-    
-    %% svm for Covariance
-    cov_of_all_events       = covFromCellArrayOfEvents( Events );
-    data_for_classifier_cov = prepareForClassification( cov_of_all_events );
-    showSvmResults( data_for_classifier_cov, vClass, 'Covariance' ,base_func);
+funcs = {@covFromCellArrayOfEvents, @correlationFromCellArrayOfEvents, @partialCorrelationFromCellArrayOfEvents};
+funcs_names = {"Covariance", "Correlation", "Partial Correlation"};
+events_names = {" on Furier transform", " on time series"};
+events_cell = {Events, F_Events};
 
-    %% svm for Correlation 
-    corr_of_all_events              = correlationFromCellArrayOfEvents( Events );
-    data_for_classifier_correlation = prepareForClassification( corr_of_all_events );
-    showSvmResults( data_for_classifier_cov, vClass, 'Correlation', base_func );
+for ii = 1:length(events_cell)% Fourier or not
+    for func_idx=1:length(funcs)% Covarience Correlation or Partial Correlation
+        data_for_classifier = prepareForClassification( funcs{func_idx}(events_cell{ii}) );
+        for base_func=["gaussian" "linear" "polynomial"]% SVM Kernal
+            showSvmResults( data_for_classifier, vClass, funcs_names{func_idx}+events_names{ii} ,base_func);
+        end
+    end  
+end
 
-    %% svm for partialCorrelation 
-    partial_corr_of_all_events          = partialCorrelationFromCellArrayOfEvents( Events );
-    data_for_classifier_par_correlation = prepareForClassification( partial_corr_of_all_events );
-    showSvmResults( data_for_classifier_par_correlation, vClass, 'partialCorrelation', base_func );
-
-    %% svm for Fourier  Covariance
-    F_cov_of_all_events       = covFromCellArrayOfEvents( F_Events );
-    F_data_for_classifier_cov = prepareForClassification( F_cov_of_all_events );
-    showSvmResults( F_data_for_classifier_cov, vClass, 'Fourier Covariance', base_func );
-
-    %% svm for Fourier  Correlation 
-    F_corr_of_all_events              = correlationFromCellArrayOfEvents( F_Events );
-    F_data_for_classifier_correlation = prepareForClassification( F_corr_of_all_events );
-    showSvmResults( F_data_for_classifier_cov, vClass, 'Fourier Correlation', base_func );
-
-    %% svm for Fourier  partialCorrelation 
-    F_partial_corr_of_all_events          = partialCorrelationFromCellArrayOfEvents( F_Events );
-    F_data_for_classifier_par_correlation = prepareForClassification( F_partial_corr_of_all_events );
-    showSvmResults( F_data_for_classifier_par_correlation, vClass, 'Fourier partialCorrelation', base_func );
-
-end 
 
