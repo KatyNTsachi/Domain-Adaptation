b83b7c87b051fa6095ca74aec4ccb5f5d2b29ce9
tsachiblau <tsachiblau@gmail.com>
2019-02-20 22:07:39 +0200

    wave changes

--- addWavesToData.m
+++ addWavesToData.m
@@ -1,57 +1,58 @@
 function [ c_data_for_classifier, c_description_for_data ] = addWavesToData(    c_data_for_classifier, c_description_for_data,...
                                                                                 c_data_to_add_waves  , c_data_to_add_waves_description,...
                                                                                 waves_size )
-    
-    for ii = 1 : length(c_data_to_add_waves)
+    for wave_size = waves_size
         
-        data_i     = c_data_to_add_waves{ii};
-        events_num = length(data_i);
-        
-        %-- check if exists
-        file_name = c_data_to_add_waves_description{ii}; 
-        file_path = "../data/" + file_name ;
-        
-        %-- if exists, load
-        if exist( file_path + ".mat", 'file' )
-            
-            tmp = load(  file_path + ".mat" );
-            res = tmp.res; 
-        
-        %-- if not, calc and save
-        else
-            
-            %-- substract mean of original events
-            c_data_zero_mean = {};
-            
-            for jj = 1: events_num
-                
-                c_data_zero_mean{jj} = data_i{jj} - mean(data_i{jj});
-                
+        for ii = 1 : length(c_data_to_add_waves)
+
+            data_i     = c_data_to_add_waves{ii};
+            events_num = length(data_i);
+
+            %-- check if exists
+            file_name = c_data_to_add_waves_description{ii} + "wave size: " + num2str(wave_size); 
+            file_path = "../data/" + file_name ;
+
+            %-- if exists, load
+            if exist( file_path + ".mat", 'file' )
+
+                tmp = load(  file_path + ".mat" );
+                res = tmp.res; 
+
+            %-- if not, calc and save
+            else
+
+                %-- substract mean of original events
+                c_data_zero_mean = {};
+
+                for jj = 1: events_num
+
+                    c_data_zero_mean{jj} = data_i{jj} - mean(data_i{jj});
+
+                end
+
+                %-- add window in time features
+                c_data_zero_mean_with_waves = addTimeWindowChanels( c_data_zero_mean, waves_size );
+
+                %-- calc covarience
+                chanels_num = size(c_data_zero_mean_with_waves{1}, 2);
+                covEvents   = nan(chanels_num, chanels_num, events_num);
+                covEvents = [];
+
+                for nn = 1 : events_num
+
+                    covEvents(:,:,nn) = c_data_zero_mean_with_waves{nn}'*c_data_zero_mean_with_waves{nn};  
+
+                end
+
+                res = prepareForClassification( covEvents );
+                save( file_path + ".mat", 'res' );
+
             end
 
-            %-- add window in time features
-            c_data_zero_mean_with_waves = addTimeWindowChanels( c_data_zero_mean, waves_size );
+            %-- add to return cell array
+            c_data_for_classifier{ length(c_data_for_classifier) + 1 }   = res;
+            c_description_for_data{ length(c_description_for_data) + 1 } = c_data_to_add_waves_description{ii};
 
-            %-- calc covarience
-            chanels_num = size(c_data_zero_mean_with_waves{1}, 2);
-            covEvents   = nan(chanels_num, chanels_num, events_num);
-            covEvents = [];
-            
-            for nn = 1 : events_num
-                
-                covEvents(:,:,nn) = c_data_zero_mean_with_waves{nn}'*c_data_zero_mean_with_waves{nn};  
-                
-            end
-            
-            res = prepareForClassification( covEvents );
-            save( file_path + ".mat", 'res' );
-            
         end
-        
-        %-- add to return cell array
-        c_data_for_classifier{ length(c_data_for_classifier) + 1 }   = res;
-        c_description_for_data{ length(c_description_for_data) + 1 } = c_data_to_add_waves_description{ii};
-        
     end
-
 end
--- useSvmClassifier.m
+++ useSvmClassifier.m
@@ -7,37 +7,24 @@ subject           = 8;
 [Events, vClass]  = GetEvents( subject, day );
 N                 = length(Events);
 %-- Create STFT Events
-STFTEvents            = getSTFTEvents(Events, vClass);
-STFTEvents_whole_time = getSTFTEventsWholeTime(Events, vClass);
 %-- Create Forier Events 
 parfor ii = 1 : N
     F_Events{ii} = abs( fft( Events{ii} ) );
 end
 
-%% preprocessing - dimentionality reduction
-acc = 0.99;
-[pca_reduce_data_Events               , pca_mat1] = pcaReduceData( Events               , acc, "EventsAfterPca");
-[pca_reduce_data_STFTEvents           , pca_mat2] = pcaReduceData( STFTEvents           , acc, "STFTEventsAfterPca");
-[pca_reduce_data_STFTEvents_whole_time, pca_mat3] = pcaReduceData( STFTEvents_whole_time, acc, "STFTEvents_whole_timeAfterPca" );
-[pca_reduce_data_F_Events             , pca_mat4] = pcaReduceData( F_Events             , acc, "F_EventsAfterPca" );
-
 %% preprocessing - add more features
 
-extended_pca_reduce_data_Events                = extendData(pca_reduce_data_Events);
-extended_pca_reduce_data_STFTEvents            = extendData(pca_reduce_data_STFTEvents);
-extended_pca_reduce_data_STFTEvents_whole_time = extendData(pca_reduce_data_STFTEvents_whole_time);
-extended_pca_reduce_data_F_Events              = extendData(pca_reduce_data_F_Events);
-extended_data_Events                           = extendData(Events);
-extended_data_STFTEvents                       = extendData(STFTEvents);
-extended_data_F_Events                         = extendData(F_Events);
+extended_data_Events   = extendData(Events);
+extended_data_F_Events = extendData(F_Events);
 
 %% doing covariance correlation and partial correlation
 c_data_for_classifier  = {};
 c_description_for_data = {};
 % funcs       = { @covFromCellArrayOfEvents, @correlationFromCellArrayOfEvents, @partialCorrelationFromCellArrayOfEvents };
 % funcs_names = { "Covariance"             , "Correlation"                    , "Partial Correlation"  };
-funcs       = { @covFromCellArrayOfEvents};
-funcs_names = { "Covariance"};
+funcs       = { @covFromCellArrayOfEvents, @correlationFromCellArrayOfEvents, @partialCorrelationFromCellArrayOfEvents };
+funcs_names = { "Covariance"             , "Correlation"                    , "Partial Correlation"  };
+
 
 % events_names    = { "on time series"                                   , "on STFT transform",...
 %                     "on STFTEvents whole_time"                         , "F Events",...
@@ -59,29 +46,17 @@ funcs_names = { "Covariance"};
 
 
 
-events_names    = { "on time series"                                   , "on STFT transform",...
-                    "F Events"                                         ,...
-                    "on pca_reduce data Events"                        , "pca reduce data STFTEvents",...
-                    "on pca_reduce data STFTEvents whole time"         , "pca reduce data F Events",...
-                    "on extended pca reduce data Events"               , "on extended pca reduce data STFTEvents",...
-                    "on extended pca reduce data STFTEvents whole time", "on extended pca reduce data F Events",...
-                    "on extended data Events"                          , "on extended data STFTEvents",...
-                    "on extended data F Events"};
+events_names    = { "on time series"                                   , "F Events"                                         ,...
+                    "on extended data Events"                          , "on extended data F Events"};
 
-events_cell     = { Events                                         , STFTEvents,...
-                    F_Events                                       ,...
-                    pca_reduce_data_Events                         , pca_reduce_data_STFTEvents,...
-                    pca_reduce_data_STFTEvents_whole_time          , pca_reduce_data_F_Events,...
-                    extended_pca_reduce_data_Events                , extended_pca_reduce_data_STFTEvents,...
-                    extended_pca_reduce_data_STFTEvents_whole_time , extended_pca_reduce_data_F_Events,...
-                    extended_data_Events                           , extended_data_STFTEvents,...
-                    extended_data_F_Events};
+events_cell     = { Events                                         , F_Events                                       ,...
+                    extended_data_Events                           , extended_data_F_Events};
 
 
 
 %set base functions
 % all_base_functions = ["linear", "gaussian", "polynomial"];
-all_base_functions = ["linear"];
+all_base_functions = ["linear", "gaussian", "polynomial"];
 
 %extract the features
 [c_data_for_classifier, c_description_for_data] = extractFeatures( events_cell, events_names,...
@@ -89,15 +64,17 @@ all_base_functions = ["linear"];
 
 %% extend data - orr suggestion
 
-c_data_to_add_waves             = { Events                  , STFTEvents,...
-                                    F_Events };
-c_data_to_add_waves_description = { "Events time with waves", "short time furier Events time with waves",...
-                                    "furier Events time with waves" };
-                                
-waves_size = 31;
-[c_data_for_classifier, c_description_for_data] = addWavesToData(   c_data_for_classifier, c_description_for_data,...
-                                                                    c_data_to_add_waves   , c_data_to_add_waves_description,...
-                                                                    waves_size );
+c_data_to_add_waves             = { Events                  , F_Events };
+c_data_to_add_waves_description = { "Events time with waves", "furier Events time with waves" };
+
+                               
+waves_size = [10, 20, 30, 40, 50, 60];
+[ c_data_for_classifier_with_waves,...
+  c_description_for_data_with_waves] = addWavesToData(   c_data_for_classifier,...
+                                                         c_description_for_data,...
+                                                         c_data_to_add_waves,...
+                                                         c_data_to_add_waves_description,...
+                                                         waves_size );
 
 %% preprocessing - dimentionality reduction
 epsilon = -0.0001;
@@ -107,12 +84,12 @@ c_short_classifier             = {};
 c_short_classifier_description = {};
 c_bad_features                 = {};
 
-for jj = 1 : length(c_data_for_classifier)
+for jj = 1 : length(c_data_for_classifier_with_waves)
     
     for ii = 1:length( min_dim_number )
 
-        [ v_short_classifier, bad_features ] = AddImportantDimensions(  c_data_for_classifier{jj},...
-                                                                        c_description_for_data{jj},...
+        [ v_short_classifier, bad_features ] = AddImportantDimensions(  c_data_for_classifier_with_waves{jj},...
+                                                                        c_description_for_data_with_waves{jj},...
                                                                         vClass,...
                                                                         epsilon,...
                                                                         min_dim_number(ii) );
@@ -128,39 +105,39 @@ end
 
 
 %% combine data
-
-[ c_combine_data, c_description ] = combineFeatures( c_short_classifier );
-
-%% create one data
-
- [ c_combined_data, c_combined_description ] = combineThreeCellArray(   c_data_for_classifier, c_description_for_data,...
-                                                                        c_short_classifier, c_short_classifier_description,...
-                                                                        c_combine_data, c_description);
-
-
-%% just run svm
-%get svm loss for the funcs and input the we set up here
+% 
+% [ c_combine_data, c_description ] = combineFeatures( c_short_classifier );
+% 
+% %% create one data
+% 
+%  [ c_combined_data, c_combined_description ] = combineThreeCellArray(   c_data_for_classifier, c_description_for_data,...
+%                                                                         c_short_classifier, c_short_classifier_description,...
+%                                                                         c_combine_data, c_description);
+% 
+% 
+% %% just run svm
+% %get svm loss for the funcs and input the we set up here
+% % table_to_show = [];
+% % table_to_show = calcSvmLoss( c_short_classifier, vClass,...
+% %                              c_description_for_data, all_base_functions,...
+% %                              table_to_show)
+% 
 % table_to_show = [];
-% table_to_show = calcSvmLoss( c_short_classifier, vClass,...
-%                              c_description_for_data, all_base_functions,...
+% table_to_show = calcSvmLoss( c_combined_data, vClass,...
+%                              c_combined_description, all_base_functions,...
 %                              table_to_show)
-
-table_to_show = [];
-table_to_show = calcSvmLoss( c_combined_data, vClass,...
-                             c_combined_description, all_base_functions,...
-                             table_to_show)
-
-
-%% run svm on all training - not very interesting
-%get svm loss for the funcs and input the we set up here
+% 
+% 
+% %% run svm on all training - not very interesting
+% %get svm loss for the funcs and input the we set up here
+% % table_to_show = [];
+% % table_to_show = calcSvmLossAllTraining(  c_short_classifier, vClass,...
+% %                                          c_description_for_data, all_base_functions,...
+% %                                          table_to_show)
+%                          
+% 
+% 
 % table_to_show = [];
-% table_to_show = calcSvmLossAllTraining(  c_short_classifier, vClass,...
-%                                          c_description_for_data, all_base_functions,...
+% table_to_show = calcSvmLossAllTraining(  c_combined_data, vClass,...
+%                                          c_combined_description, all_base_functions,...
 %                                          table_to_show)
-                         
-
-
-table_to_show = [];
-table_to_show = calcSvmLossAllTraining(  c_combined_data, vClass,...
-                                         c_combined_description, all_base_functions,...
-                                         table_to_show)
