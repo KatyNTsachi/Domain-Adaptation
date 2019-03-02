2e146517c8474063dac4c26f57b60a502267dabd
katiePodiatchev <katy1427@gmail.com>
2019-03-02 16:31:43 +0200

    working on network

--- checkDeepOneWave.m
+++ checkDeepOneWave.m
@@ -1,97 +1,135 @@
 close all
 clear
-addpath("./functions")
+addpath("./functions/")
 
-%% prepare for calc 
-day               = 2;
-subject           = 8;
-for day = 1:
-[Events, vClass]  = GetEvents( subject, day );
-N                 = length(Events);
-%-- Create STFT Events
-%-- Create Forier Events 
-parfor ii = 1 : N
-    F_Events{ii} = abs( fft( Events{ii} ) );
+%% --prepare for calc 
+clc
+
+vClass = [];
+Events = {};
+for day = 1:num_days
+    for subject = 1:num_subjects
+        [tmp_Events, tmp_vClass]  = GetEvents( subject, day );
+        Events = combineTwoCellArrays(Events,tmp_Events);
+        vClass = [vClass; tmp_vClass];
+    end
 end
 
-%% preprocessing - add more features
+%% --Devide to test training and            
+m_data   = cell2mat(Events);
+v_lables = kron(vClass, ones(22,1));
+max_diff = 10;
+while true
+    b_training   = rand( size(v_lables,1),1 ) > 0.3;
+    b_test       = ~b_training;
 
-extended_data_Events   = extendData(Events);
-extended_data_F_Events = extendData(F_Events);
+    %--test our division
+    class_one   = sum(v_lables(b_training)==1);
+    class_two   = sum(v_lables(b_training)==2);
+    class_three = sum(v_lables(b_training)==3);
+    class_four  = sum(v_lables(b_training)==4);
+    avg = (class_one + class_two + class_three + class_four)/4;
+    if abs(class_one - avg)< max_diff && abs(class_two - avg)< max_diff && abs(class_three - avg)< max_diff && abs(class_four - avg)< max_diff
+        break;
+    end
+end
 
-%% doing covariance correlation and partial correlation
-c_data_for_classifier  = {};
-c_description_for_data = {};
-% funcs       = { @covFromCellArrayOfEvents, @correlationFromCellArrayOfEvents, @partialCorrelationFromCellArrayOfEvents };
-% funcs_names = { "Covariance"             , "Correlation"                    , "Partial Correlation"  };
-funcs       = { @covFromCellArrayOfEvents, @correlationFromCellArrayOfEvents, @partialCorrelationFromCellArrayOfEvents };
-funcs_names = { "Covariance"             , "Correlation"                    , "Partial Correlation"  };
+m_train_data  = m_data(:,b_training);
+v_train_lable = v_lables(b_training);
+m_test_data   = m_data(:,b_test);
+v_test_lable  = v_lables(b_test);
 
+%-- shaffel data
+test_perm  = randperm(length(v_test_lable));
+train_perm = randperm(length(v_train_lable));
+%val_perm   = randperm(length(v_val_lable));
 
-% events_names    = { "on time series"                                   , "on STFT transform",...
-%                     "on STFTEvents whole_time"                         , "F Events",...
-%                     "on pca_reduce data Events"                        , "pca reduce data STFTEvents",...
-%                     "on pca_reduce data STFTEvents whole time"         , "pca reduce data F Events",...
-%                     "on extended pca reduce data Events"               , "on extended pca reduce data STFTEvents",...
-%                     "on extended pca reduce data STFTEvents whole time", "on extended pca reduce data F Events",...
-%                     "on extended data Events"                          , "on extended data STFTEvents",...
-%                     "on extended data F Events"                        , "on extended data Events with const fetures"};
+m_train_data  = m_train_data(:,train_perm);
+v_train_lable = v_train_lable(train_perm);
+m_test_data   = m_test_data(:,test_perm);
+v_test_lable  = v_test_lable(test_perm);
+%m_val_data   = m_test_data(:,test_perm);
+%v_val_lable  = v_test_lable(test_perm);
 
-% events_cell     = { Events                                         , STFTEvents,...
-%                     STFTEvents_whole_time                          , F_Events,...
-%                     pca_reduce_data_Events                         , pca_reduce_data_STFTEvents,...
-%                     pca_reduce_data_STFTEvents_whole_time          , pca_reduce_data_F_Events,...
-%                     extended_pca_reduce_data_Events                , extended_pca_reduce_data_STFTEvents,...
-%                     extended_pca_reduce_data_STFTEvents_whole_time , extended_pca_reduce_data_F_Events,...
-%                     extended_data_Events                           , extended_data_STFTEvents,...
-%                     extended_data_F_Events                         , extended_data_Events_with_const_fetures};
+%% cut hte data
 
-
-
-events_names    = { "on time series"                                   , "F Events"                                         ,...
-                    "on extended data Events"                          , "on extended data F Events"};
-
-events_cell     = { Events                                         , F_Events                                       ,...
-                    extended_data_Events                           , extended_data_F_Events};
-
-
-
-%set base functions
-% all_base_functions = ["linear", "gaussian", "polynomial"];
-all_base_functions = ["linear", "gaussian", "polynomial"];
-
-%extract the features
-[c_data_for_classifier, c_description_for_data] = extractFeatures( events_cell, events_names,...
-                                                                   funcs, funcs_names, vClass );
-
-%% extend data rectangular wave
-
-c_data_to_add_waves             = { Events                  , F_Events };
-c_data_to_add_waves_description = { "Events time with waves", "furier Events time with waves" };
-
-                               
-waves_size = [10, 20, 30, 40, 50, 60];
-[ c_data_for_classifier_with_rec_waves,...
-  c_description_for_data_with_rec_waves] = covWIthConstFeatures(   c_data_to_add_waves,...
-                                                                   c_data_to_add_waves_description,...
-                                                                   @addTimeWindowChanels, waves_size );
-                                                               
-sigma = [50 ,51];
-[ c_data_for_classifier_with_gau_waves,...
-  c_description_for_data_with_gau_waves] = covWIthConstFeatures(   c_data_to_add_waves,...
-                                                                   c_data_to_add_waves_description,...
-                                                                   @addGaussianWindowCannels, sigma );
+m_train_data  = m_train_data(:,1:500);
+v_train_lable = v_train_lable(1:500);
+m_val_data    = m_train_data
+v_val_lable   = v_train_lable
 
 
+%% define network structure
 
-%% create one data
-[ c_combined_data, c_combined_description ] = combineThreeCellArray(   c_data_for_classifier, c_description_for_data,...
-                                                                       c_data_for_classifier_with_rec_waves, c_description_for_data_with_rec_waves,...
-                                                                       c_data_for_classifier_with_gau_waves, c_description_for_data_with_gau_waves);
+layers = [
+    
+    imageInputLayer([int32(size(m_train_data,1)), int32(1)])
+    %-- layer 1
+    convolution2dLayer([3 1], 4,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    maxPooling2dLayer([2 1],'Stride',2)
+    
+    %-- layer 2
+    convolution2dLayer([3 1], 8,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    maxPooling2dLayer([2 1],'Stride',2)
+    
+    
+    %-- layer 3
+    convolution2dLayer([3 1], 16,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    maxPooling2dLayer([2 1],'Stride',2)
+    
+    %-- layer 4
+    convolution2dLayer([3 1], 32,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    maxPooling2dLayer([2 1],'Stride',2)
+    
+    %-- layer 5
+    convolution2dLayer([3 1], 64,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    maxPooling2dLayer([2 1],'Stride',2)
+    
+    %-- layer 6
+    convolution2dLayer([3 1], 128,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+       
+    %-- fully connected
+    fullyConnectedLayer(4)
+    softmaxLayer
+    classificationLayer];
+    
+%% training options
 
-%% calc svm
-table_to_show = [];
-table_to_show = calcSvmLossTnV( c_combined_data, vClass,...
-                                c_combined_description, all_base_functions,...
-                                table_to_show);
+v_train_lable_categorical = discretize(v_train_lable,[0.5 1.5 2.5 3.5 4.5],...
+                                       'categorical', {'1' '2' '3' '4'});
+tmp_m_train_data = reshape(m_train_data,[750,1,1,500]);     
+
+options = trainingOptions('sgdm', ...
+    'InitialLearnRate',0.01, ...
+    'MaxEpochs',100, ...
+    'Shuffle','every-epoch', ...
+    'ValidationData',{tmp_m_train_data, v_train_lable_categorical}, ...
+    'ValidationFrequency',30, ...
+    'Verbose',false, ...
+    'Plots','training-progress');
+
+%% train 
+
+%c_train_data = mat2cell(m_train_data,[750],ones(500,1));
+net = trainNetwork( tmp_m_train_data, v_train_lable_categorical, layers, options );
+
+%% test accuracy
+
+YPred       = classify(net,tmp_m_train_data);
+YValidation = v_train_lable_categorical;
+
+accuracy = sum(YPred == YValidation)/numel(YValidation)
+
 
