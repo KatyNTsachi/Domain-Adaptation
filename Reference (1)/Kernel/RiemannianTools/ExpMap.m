0f0135db8193a6f2721277b6dcae992b3252e196
tsachiblau <tsachiblau@gmail.com>
2019-03-29 15:28:16 +0300

    sort files

--- Reference (1)/Kernel/RiemannianTools/ExpMap.m
+++
@@ -1,5 +0,0 @@
-function P = ExpMap(B, S)
-    BP = B^(1/2);
-    BN = B^(-1/2);
-    P  = BP * expm(BN * S * BN) * BP;
-end
--- functions (1)/splitInTime.m
+++
@@ -1,9 +0,0 @@
-function [Events] = splitInTime(Events, window_size)
-    %SPLITINTIME Summary of this function goes here
-    %   Detailed explanation goes here
-   
-    for ii = 1:length(Events)
-        Events{ii} = reshape(Events{ii}, window_size, []);
-    end
-end
-
